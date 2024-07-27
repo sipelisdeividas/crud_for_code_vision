6709abfca0061e8f4b3aa827692ee1cd44ae9341
sipelisdeividas <deividasprogrameris@gmail.com>
2024-07-27 16:17:15 +0300

    Adding Line Spaces

--- app/Services/Helpers/PriceClassifier.php
+++ app/Services/Helpers/PriceClassifier.php
@@ -4,28 +4,45 @@ namespace App\Services\Helpers;
 
 class PriceClassifier
 {
-    private const PRICE_LABELS = [
+    private const PRICE_LABELS =
+    [
         'LOW' => 'Žema',
+
         'MEDIUM' => 'Vidutinė',
+
         'HIGH' => 'Aukšta',
+
         'VERY_HIGH' => 'Labai Aukšta'
     ];
 
-    private const PRICE_RANGES = [
+    private const PRICE_RANGES =
+    [
         'LOW' => 50000,
+
         'MEDIUM' => 150000,
+
         'HIGH' => 500000
     ];
 
     public function classify(float $price): string
     {
-        if ($price <= self::PRICE_RANGES['LOW']) {
+        if ($price <= self::PRICE_RANGES['LOW'])
+        {
             return self::PRICE_LABELS['LOW'];
-        } elseif ($price <= self::PRICE_RANGES['MEDIUM']) {
+        }
+
+        elseif ($price <= self::PRICE_RANGES['MEDIUM'])
+        {
             return self::PRICE_LABELS['MEDIUM'];
-        } elseif ($price <= self::PRICE_RANGES['HIGH']) {
+        }
+
+        elseif ($price <= self::PRICE_RANGES['HIGH'])
+        {
             return self::PRICE_LABELS['HIGH'];
-        } else {
+        }
+
+        else
+        {
             return self::PRICE_LABELS['VERY_HIGH'];
         }
     }
--- app/Services/ProductPriceService.php
+++ app/Services/ProductPriceService.php
@@ -22,6 +22,7 @@ class ProductPriceService implements ProductPriceServiceInterface
             ],
             [
                 'price' => $price,
+
                 'value_class' => $valueClass,
             ]
         );
